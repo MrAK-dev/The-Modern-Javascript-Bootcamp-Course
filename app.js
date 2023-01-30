72511e9d30db81fec56ecd7fa6817a9ab2b1ea71
Alex <alex.fiendish@gmail.com>
2023-01-30 19:00:49 +0200

    added some code examples of how to get elements from HTML

---
+++ app.js
@@ -0,0 +1,18 @@
+const img = document.getElementById('bear-photo');
+const paragraph = document.getElementById('main');
+
+const paragraphs = document.getElementsByTagName('p');
+const inputs = document.getElementsByTagName('input');
+
+for (let input of inputs) {
+  console.log(input.value);
+}
+
+const heading = document.getElementsByClassName('header');
+const ul = document.getElementsByTagName('ul')[0];
+ul.getElementsByTagName('li');
+
+document.querySelector('h1');
+document.querySelector('section ul li.special');
+document.querySelectorAll('.special');
+document.querySelectorAll('li');
