4f53a8c5cb6dcc034454ff692a059d9298e67b6f
Alex <alex.fiendish@gmail.com>
2023-02-14 22:09:54 +0200

    added authentication and password encryption

--- repositories/users.js
+++ repositories/users.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const crypto = require('crypto');
+const util = require('util');
 
+const scrypt = util.promisify(crypto.scrypt);
 class UsersRepository {
   constructor(filename) {
     if (!filename) {
@@ -24,11 +26,28 @@ class UsersRepository {
   }
 
   async create(attrs) {
+    //  attrs === {email: '', passwrod: ''}
     attrs.id = this.randomId();
+    const salt = crypto.randomBytes(8).toString('hex');
+    const buf = await scrypt(attrs.password, salt, 64);
+
     const records = await this.getAll();
-    records.push(attrs);
+    const record = { ...attrs, password: `${buf.toString('hex')}.${salt}` };
+    records.push(record);
 
     await this.writeAll(records);
+
+    return record;
+  }
+
+  async comparePasswords(saved, supplied) {
+    //  Saved -> passwrod saved in our database. 'hashed.salt'
+    //  Supplied -> password given to us by a user trying sign in
+
+    const [hashed, salt] = saved.split('.');
+    const hashedSuppliedBuf = await scrypt(supplied, salt, 64);
+
+    return hashed === hashedSuppliedBuf.toString('hex');
   }
 
   async writeAll(records) {
