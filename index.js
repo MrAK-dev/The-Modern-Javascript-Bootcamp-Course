f4c85665879ab818d93d9e9814cb7ea4d5835618
Alex <alex.fiendish@gmail.com>
2023-02-14 19:53:02 +0200

    added users storage, sign up validation

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const usersRepo = require('./repositories/users');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,8 +18,16 @@ app.get('/', (req, res) => {
   `);
 });
 
-app.post('/', (req, res) => {
-  console.log(req.body);
+app.post('/', async (req, res) => {
+  const { email, 'password ': password, passwordConfirmation } = req.body;
+  const existingUser = await usersRepo.getOneBy({ email });
+  if (existingUser) {
+    return res.send('Email in use');
+  }
+
+  if (password !== passwordConfirmation) {
+    return res.send('Passwords must match!');
+  }
   res.send('Account created!');
 });
 
